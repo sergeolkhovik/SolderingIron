931f04465d198e2e2c3c40fc9d76ba0230c74403
Serge Olkhovik <serge.olkhovik@gmail.com>
2018-03-28 23:04:23 +0300

    Enable/disable debug output. 2 buttons with predefined values support. Some temperature calibration. Code updates. Fix in schema.

--- SolderingIron.ino
+++ SolderingIron.ino
@@ -9,6 +9,8 @@ char dash[]      = { "---" };
 char hotMsg[]    = { "HOT" };
 char errMsg[]    = { "ERR" };
 
+#define DEBUG 1
+
 #define E_OK  0
 #define E_ALL 1
 #define E_HOT 2
@@ -19,6 +21,11 @@ char errMsg[]    = { "ERR" };
 #define TEMP 3  // current temperature
 #define HOTT 7
 #define OUTT 10 // temperature control
+
+#define TEMP_A 250
+#define TEMP_B 350
+#define TEMP_BUTTON_A 4 // predefined temperature, button A
+#define TEMP_BUTTON_B 5
 
 #define ENCT 800 // encoder value threshold
 #define ENCB_DELAY 500
@@ -32,13 +39,15 @@ char errMsg[]    = { "ERR" };
 
 #define HOTL_DELAY 5000
 #define HOTL_HOT 390
-#define HOTL_ERR 900
+#define HOTL_ERR 520
 
 #define CUR_TEMP_DELAY 1000
 #define SET_TEMP_DELAY 5000
 
-int encL = 1, encR = 1, encB = 1, lastEncB = 1;
+int encL = 1, encR = 1, encB = 1, offMode = 1;
 unsigned long encBTimer = 0;
+unsigned long tempATimer = 0;
+unsigned long tempBTimer = 0;
 
 unsigned long setTempTimer = 0;
 
@@ -47,7 +56,7 @@ int setTemp = MIN_TEMP;
 // 1 - preparing
 // 2 - left
 // 3 - right
-int modeSetTemp = 0;
+int setTempMode = 0;
 
 int curTemp = 0, dispTemp = 0, curOutTemp = 0;
 unsigned long curTempTimer = 0;
@@ -61,7 +70,7 @@ void setup() {
   Serial.begin(250000);
    
   sevseg.begin(COMMON_ANODE, digits, digitPins, segPins);
-  sevseg.setBrightness(50);
+  sevseg.setBrightness(100);
 
   pinMode(OUTT, OUTPUT);
   analogWrite(OUTT, 0);
@@ -80,8 +89,6 @@ void loop() {
   encL = getEncValue(ENCL);
   encR = getEncValue(ENCR);
   encB = getEncValue(ENCB); // 1000?
-//  encB = analogRead(ENCB);
-//  encB = encB > 1000 ? 1 : 0;
 
   int h = analogRead(HOTT);
   if (h < HOTL_HOT) {
@@ -99,11 +106,13 @@ void loop() {
   if (error == E_OK && (curTemp == 0 || curTempTimer == 0)) {
     int t = analogRead(3);
     float v = t * 4.9;
-    Serial.print("v = "); Serial.print(v); Serial.print(", t = "); Serial.println(t);
+    if (DEBUG) {
+      Serial.print("v = "); Serial.print(v); Serial.print(", t = "); Serial.println(t);
+    }
     //if (t > MAX_TEMP) t = MAX_TEMP; // test
     //if (t < (curTemp - DIF_TEMP) || t > (curTemp + DIF_TEMP)) {
     t = map(t, 0, 1023, 0, 5000);
-    t = map(t, 0, 1200, 20, 400);
+    t = map(t, 0, 3600, 20, 400); // original value - 1800
 //    t *= FIX_TEMP;
     // if (t > 0) {
     //  curTemp = t;
@@ -112,7 +121,9 @@ void loop() {
     if (t > ERR_TEMP) {
       error = E_ALL;
     } else {
-Serial.print("he = "); Serial.println(h);
+//      if (DEBUG) {
+//        Serial.print("he = "); Serial.println(h);
+//      }
       dispTemp = curTemp = t;
       for (i = 0; i < TEMP_HIST - 1; i++) {
         tempHist[i] = tempHist[i+1];
@@ -120,14 +131,18 @@ Serial.print("he = "); Serial.println(h);
       }
       tempHist[TEMP_HIST-1] = curTemp;
       dispTemp /= TEMP_HIST;
-  
-      Serial.print("T = "); Serial.print(curTemp); Serial.print(" / "); Serial.println(dispTemp);
+
+      if (DEBUG) {
+        Serial.print("T = "); Serial.print(curTemp); Serial.print(" / "); Serial.println(dispTemp);
+      }
       curTempTimer = millis();
     }
   }
 
   if (error != E_OK) {
-    Serial.print("he = "); Serial.println(h);
+    if (DEBUG) {
+      Serial.print("he = "); Serial.println(h);
+    }
     switch (error) {
       case E_HOT:
         sevseg.setChars(hotMsg);
@@ -137,7 +152,7 @@ Serial.print("he = "); Serial.println(h);
         break;
     }
     sevseg.refreshDisplay();
-    lastEncB = 1;
+    offMode = 1;
     analogWrite(OUTT, 0);
     return;
   }
@@ -147,44 +162,68 @@ Serial.print("he = "); Serial.println(h);
   }
   if (encB == 1 && encBTimer > 0 && millis() > (encBTimer + ENCB_DELAY)) {
     encBTimer = 0;
-    lastEncB = lastEncB ? 0 : 1;
-    if (lastEncB == 0) {
-      for (i = 0; i < TEMP_HIST; i++) {
+    offMode = offMode ? 0 : 1;
+    if (offMode == 0) {
+      for (i = 0; i < TEMP_HIST; i++) { //  fill history buffer with current temperature
         tempHist[i] = curTemp;
       }    
       dispTemp = curTemp;
     }
   }
 
-  if (lastEncB == 0) {
-    switch (modeSetTemp) {
+  if (encB == 1) {
+    int b = getEncValue(TEMP_BUTTON_A);
+    if (b == 0) {
+      tempATimer = millis();
+    }
+    if (b == 1 && tempATimer > 0 && millis() > (tempATimer + ENCB_DELAY)) {
+      tempATimer = 0;
+      setTemp = TEMP_A;
+      setTempTimer = millis();
+      offMode = 0;
+    }
+    b = getEncValue(TEMP_BUTTON_B);
+    if (b == 0) {
+      tempBTimer = millis();
+    }
+    if (b == 1 && tempBTimer > 0 && millis() > (tempBTimer + ENCB_DELAY)) {
+      tempBTimer = 0;
+      setTemp = TEMP_B;
+      setTempTimer = millis();
+      offMode = 0;
+    }
+  }
+
+  if (offMode == 0) {
+    switch (setTempMode) {
       case 0:
         if (encL == 0 && encR == 0) {
-          modeSetTemp = 1;
+          setTempMode = 1;
         }
         break;
       case 1:
         if (encL == 1 && encR == 0) {
-          modeSetTemp = 2;
+          setTempMode = 2;
         } else if (encL == 0 && encR == 1) {
-          modeSetTemp = 3;
+          setTempMode = 3;
         }
         break;
       case 2:
         if (encR == 1) {
           setTemp += SHF_TEMP;
-          modeSetTemp = 0;
+          setTempMode = 0;
           setTempTimer = millis();
         }
         break;
       case 3:
         if (encL == 1) {
           setTemp -= SHF_TEMP;
-          modeSetTemp = 0;
+          setTempMode = 0;
           setTempTimer = millis();
         }
         break;
     }
+  
     if (setTemp < MIN_TEMP) setTemp = MIN_TEMP;
     if (setTemp > MAX_TEMP) setTemp = MAX_TEMP;
 
@@ -203,7 +242,7 @@ Serial.print("he = "); Serial.println(h);
   sevseg.refreshDisplay();
 
   int outTemp = 0;
-  if (lastEncB == 0 && curTemp < setTemp) {
+  if (offMode == 0 && curTemp < setTemp) {
     int d = setTemp - curTemp;
     if (d > 16) {
       outTemp = 230;
@@ -218,9 +257,5 @@ Serial.print("he = "); Serial.println(h);
   if (outTemp != curOutTemp) {
     analogWrite(OUTT, outTemp);
     curOutTemp = outTemp;
-//    Serial.print("lastEncB = "); Serial.print(lastEncB); Serial.print(", ");
-//    Serial.print("curTemp = "); Serial.print(curTemp); Serial.print(", ");
-//    Serial.print("setTemp = "); Serial.print(setTemp); Serial.print(", ");
-//    Serial.print("outTemp = "); Serial.print(outTemp); Serial.println("");
   }
 }
